25f53d6a442ee3c06352c66fd6ed8734faac1d1b
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-01-31 03:30:32 +0900

    アイテムクラスを少し作成

---
+++ include/game/Item.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "WorldTransform.h"
+#include "MathUtility.h"
+#include "Shape.h"
+#include "Collision2D.h"
+
+class Item {
+
+private:
+	WorldTransform m_transform;
+	Collider2D::Circle m_collider;
+
+
+public:
+
+};
